4fae293dd0642b155b6e60e51e8f06edb6be3c80
piyuxx <piyugautam2366@gmail.com>
2024-04-25 20:48:52 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,12 @@ export default function Home() {
 
   const getClickedCityCords = (lat: number, lon: number) => {
     setActiveCityCoords([lat, lon]);
-
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    if (window !== undefined) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
